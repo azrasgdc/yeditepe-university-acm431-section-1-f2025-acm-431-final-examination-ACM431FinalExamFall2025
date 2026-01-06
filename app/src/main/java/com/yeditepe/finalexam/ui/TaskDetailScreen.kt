ae6b6d5cb1faa287f672a49df0a5e1c3cdbc2980
Azra <151929115+azrasgdc@users.noreply.github.com>
2026-01-06 15:26:04 +0300

    Update TaskDetailScreen.kt

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskDetailScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskDetailScreen.kt
@@ -2,6 +2,8 @@ package com.yeditepe.finalexam.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -11,5 +13,9 @@ fun TaskDetailScreen(title: String) {
 
     Column(modifier = Modifier.padding(16.dp)) {
         // TODO 4: Display task title
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleLarge
+        )
     }
 }
