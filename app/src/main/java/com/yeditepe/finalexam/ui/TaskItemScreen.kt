259ef28700a0ae752c9817916eacb103177f2401
Azra <151929115+azrasgdc@users.noreply.github.com>
2026-01-06 15:26:24 +0300

    Update TaskItemScreen.kt

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskItemScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskItemScreen.kt
@@ -6,6 +6,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -14,7 +18,7 @@ import androidx.compose.ui.unit.dp
 fun TaskItemScreen() {
 
     // TODO 1: Create a mutable state to hold completion status (Boolean)
-    // Initial value should be false
+    var isCompleted by remember { mutableStateOf(false) }
 
     Column(modifier = Modifier.padding(16.dp)) {
 
@@ -25,10 +29,16 @@ fun TaskItemScreen() {
 
         // TODO 2: Show text "Completed" or "Not Completed"
         // depending on completion state
+        Text(
+            text = if (isCompleted) "completed" else "not completed",
+            style = MaterialTheme.typography.bodyLarge
+        )
+
 
         Button(
             onClick = {
                 // TODO 3: Toggle completion state
+                isCompleted = !isCompleted
             }
         ) {
             Text("Change Status")
@@ -40,4 +50,5 @@ fun TaskItemScreen() {
 @Composable
 fun TaskItemPreview() {
     // TODO: Call TaskItemScreen
+    TaskItemScreen()
 }
